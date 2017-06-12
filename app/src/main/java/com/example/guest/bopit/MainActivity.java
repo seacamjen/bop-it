1314be02be27297f3b9f0f3c4673a2e6ac9385e4
Cameron Jensen and Megan Warnock <megan@>
2017-06-12 15:51:17 -0700

    add sound

--- app/src/main/java/com/example/guest/bopit/MainActivity.java
+++ app/src/main/java/com/example/guest/bopit/MainActivity.java
@@ -1,21 +1,30 @@
 package com.example.guest.bopit;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.support.v4.view.GestureDetectorCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import java.util.Random;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends Activity implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener {
     @Bind(R.id.doubleTap) TextView mDoubleTap;
+    @Bind(R.id.fling) TextView mFling;
+    @Bind(R.id.longclick) TextView mLongClick;
+    private MediaPlayer mHold;
+    private MediaPlayer mDoubleClick;
+    private MediaPlayer mFlingMusic;
+
+
+
     GestureDetector gestureDetector;
 
     private static final String DEBUG_TAG = "Gestures";
@@ -27,6 +36,10 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+         mHold = MediaPlayer.create(MainActivity.this, R.raw.hold);
+         mDoubleClick = MediaPlayer.create(MainActivity.this, R.raw.doubleclick);
+         mFlingMusic = MediaPlayer.create(MainActivity.this, R.raw.fling);
+
         mDetector = new GestureDetectorCompat(this,this);
         mDetector.setOnDoubleTapListener(this);
 
@@ -37,7 +50,58 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
 //                return false;
 //            }
 //        });
+        hideDoubleTap();
+        hideFling();
+        showLongClick();
     }
+
+    public void hideDoubleTap() {
+        mDoubleTap.setVisibility(View.GONE);
+    }
+
+    public void showDoubleTap() {
+        mDoubleTap.setVisibility(View.VISIBLE);
+    }
+    public void hideFling() {
+        mFling.setVisibility(View.GONE);
+    }
+
+    public void showFling() {
+        mFling.setVisibility(View.VISIBLE);
+    }
+    public void hideLongClick() {
+        mLongClick.setVisibility(View.GONE);
+    }
+
+    public void showLongClick() {
+        mLongClick.setVisibility(View.VISIBLE);
+    }
+
+    public void randomGen() {
+        Random Generator = new Random();
+        int num1;
+        num1 = Generator.nextInt(3);
+        if ( num1 == 0) {
+            showLongClick();
+            mHold.start();
+            hideDoubleTap();
+            hideFling();
+        }
+        if (num1 == 1) {
+            showDoubleTap();
+            mDoubleClick.start();
+            hideFling();
+            hideLongClick();
+        }
+        if (num1 == 2) {
+            showFling();
+            mFlingMusic.start();
+            hideDoubleTap();
+            hideLongClick();
+        }
+    }
+
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event){
@@ -56,14 +120,18 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
     public boolean onFling(MotionEvent event1, MotionEvent event2,
                            float velocityX, float velocityY) {
         Log.d(DEBUG_TAG, "onFling: " + event1.toString()+event2.toString());
-        Toast.makeText(MainActivity.this, "Fling Time", Toast.LENGTH_LONG).show();
+//        Toast.makeText(MainActivity.this, "fling Time", Toast.LENGTH_LONG).show();
+        hideFling();
+        randomGen();
         return true;
     }
 
     @Override
     public void onLongPress(MotionEvent event) {
         Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
-        Toast.makeText(MainActivity.this, "Long Press", Toast.LENGTH_LONG).show();
+//        Toast.makeText(MainActivity.this, "Long Press", Toast.LENGTH_LONG).show();
+        hideLongClick();
+        randomGen();
     }
 
     @Override
@@ -87,14 +155,15 @@ public class MainActivity extends Activity implements GestureDetector.OnGestureL
     @Override
     public boolean onDoubleTap(MotionEvent event) {
         Log.d(DEBUG_TAG, "onDoubleTap: " + event.toString());
-        Toast.makeText(MainActivity.this, "Double Tap", Toast.LENGTH_LONG).show();
+//        Toast.makeText(MainActivity.this, "Double Tap", Toast.LENGTH_LONG).show();
+        hideDoubleTap();
+        randomGen();
         return true;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent event) {
-        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
-        Toast.makeText(MainActivity.this, "Double tap event", Toast.LENGTH_LONG).show();
+//        Log.d(DEBUG_TAG, "onDoubleTapEvent: " + event.toString());
         return true;
     }
 
